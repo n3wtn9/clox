1d9eb0a42c76f014499809c32020e965542a0ddf
newton <newton@newtons-mbp.lan>
2022-09-07 10:10:32 -0700

    Finish 26.5.

--- src/memory.c
+++ src/memory.c
@@ -146,6 +146,28 @@ static void traceReferences() {
   }
 }
 
+static void sweep() {
+  Obj* previous = NULL;
+  Obj* object = vm.objects;
+  while (object != NULL) {
+    if (object->isMarked) {
+      object->isMarked = false;
+      previous = object;
+      object = object->next;
+    } else {
+      Obj* unreached = object;
+      object = object->next;
+      if (previous != NULL) {
+        previous->next = object;
+      } else {
+        vm.objects = object;
+      }
+
+      freeObject(unreached);
+    }
+  }
+}
+
 void freeObjects() {
   Obj* object = vm.objects;
   while (object != NULL) {
@@ -164,6 +186,8 @@ void collectGarbage() {
 
   markRoots();
   traceReferences();
+  tableRemoveWhite(&vm.strings);
+  sweep();
 
 #ifdef DEBUG_LOG_GC
   printf("-- gc end\n");
